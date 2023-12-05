57d32d72cce7dbe4b07a48d659990cfae8c8b57e
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-12-05 09:36:42 -0600

    feat: add select menu testing

--- src/commands/team.rs
+++ src/commands/team.rs
@@ -3,7 +3,10 @@ use serenity::builder::{
     CreateEmbed,
     CreateCommandOption,
     CreateInteractionResponse,
-    CreateInteractionResponseMessage
+    CreateInteractionResponseMessage,
+    CreateActionRow,
+    CreateSelectMenu,
+    CreateSelectMenuKind, CreateSelectMenuOption
 };
 use serenity::all::{CommandOptionType, CommandDataOptionValue};
 use serenity::client::Context;
@@ -16,7 +19,24 @@ pub fn response(_ctx: &Context, interaction: &CommandInteraction) -> CreateInter
         None
     };
 
+    let page_menu = CreateSelectMenu::new("team_page_select", CreateSelectMenuKind::String {
+        options: vec![
+            CreateSelectMenuOption::new("Page 1", "opt_1"),
+            CreateSelectMenuOption::new("Page 2", "opt_2"),
+        ],
+    });
+    let season_menu = CreateSelectMenu::new("team_season_select", CreateSelectMenuKind::String {
+        options: vec![
+            CreateSelectMenuOption::new("Season 1", "opt_1"),
+            CreateSelectMenuOption::new("Season 2", "opt_2"),
+        ],
+    });
+
     let message = CreateInteractionResponseMessage::new()
+        .components(vec![
+            CreateActionRow::SelectMenu(page_menu),
+            CreateActionRow::SelectMenu(season_menu),
+        ])
         .add_embed(
             CreateEmbed::new()
                 .title(format!("Team {}", team_number.unwrap()))
