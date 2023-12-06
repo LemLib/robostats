d2cf65d35f762f60ac9d4be512de5b4351d68b89
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-12-05 20:54:10 -0600

    add more team info fields

--- src/commands/team.rs
+++ src/commands/team.rs
@@ -1,60 +1,67 @@
+use serenity::all::{CommandDataOptionValue, CommandOptionType, ReactionType};
 use serenity::builder::{
-    CreateCommand,
-    CreateEmbed,
-    CreateCommandOption,
-    CreateInteractionResponse,
-    CreateInteractionResponseMessage,
-    CreateActionRow,
-    CreateSelectMenu,
-    CreateSelectMenuKind, CreateSelectMenuOption
+    CreateActionRow, CreateCommand, CreateCommandOption, CreateEmbed, CreateInteractionResponse,
+    CreateInteractionResponseMessage, CreateSelectMenu, CreateSelectMenuKind,
+    CreateSelectMenuOption,
 };
-use serenity::all::{CommandOptionType, CommandDataOptionValue, ReactionType};
 use serenity::client::Context;
 use serenity::model::application::CommandInteraction;
 
 use crate::api::robotevents::client::RobotEvents;
 
-pub async fn response(_ctx: &Context, interaction: &CommandInteraction, robotevents: &RobotEvents) -> CreateInteractionResponse {
-    let team_number = if let CommandDataOptionValue::String(number) = &interaction.data.options[0].value {
-        number
-    } else {
-        return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("Invalid team number."))
-    };
+pub async fn response(
+    _ctx: &Context,
+    interaction: &CommandInteraction,
+    robotevents: &RobotEvents,
+) -> CreateInteractionResponse {
+    let team_number =
+        if let CommandDataOptionValue::String(number) = &interaction.data.options[0].value {
+            number
+        } else {
+            return CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new().content("Invalid team number."),
+            );
+        };
 
     if let Ok(teams) = robotevents.find_teams(team_number).await {
         if let Some(team) = teams.iter().next() {
             let team = team.clone();
 
-            let page_menu = CreateSelectMenu::new("team_page_select", CreateSelectMenuKind::String {
-                options: vec![
-                    CreateSelectMenuOption::new("Team Info", "team")
-                        .emoji(ReactionType::Unicode("🗿".to_string()))
-                        .description("General information about the team")
-                        .default_selection(true),
-                    CreateSelectMenuOption::new("Awards", "awards")
-                        .emoji(ReactionType::Unicode("🏆".to_string()))
-                        .description("Awards from events throughout the season"),
-                    CreateSelectMenuOption::new("Skills", "skills")
-                        .emoji(ReactionType::Unicode("📄".to_string()))
-                        .description("Skills scores"),
-                    CreateSelectMenuOption::new("Trueskill", "trueskill")
-                        .emoji(ReactionType::Unicode("📊".to_string()))
-                        .description("TrueSkill ranking from vrc-data-analysis"),
-                    CreateSelectMenuOption::new("Events", "events")
-                        .emoji(ReactionType::Unicode("🗓️".to_string()))
-                        .description("Event attendance from this team"),
-                ],
-            });
-            let season_menu = CreateSelectMenu::new("team_season_select", CreateSelectMenuKind::String {
-                options: vec![
-                    CreateSelectMenuOption::new("Season 1", "opt_1")
-                        .description("2022-2023"),
-                    CreateSelectMenuOption::new("Season 2", "opt_2")
-                        .description("2023-2024")
-                        .default_selection(true),
-                ],
-            });
-        
+            let page_menu = CreateSelectMenu::new(
+                "team_page_select",
+                CreateSelectMenuKind::String {
+                    options: vec![
+                        CreateSelectMenuOption::new("Team Info", "team")
+                            .emoji(ReactionType::Unicode("🗿".to_string()))
+                            .description("General information about the team")
+                            .default_selection(true),
+                        CreateSelectMenuOption::new("Awards", "awards")
+                            .emoji(ReactionType::Unicode("🏆".to_string()))
+                            .description("Awards from events throughout the season"),
+                        CreateSelectMenuOption::new("Skills", "skills")
+                            .emoji(ReactionType::Unicode("📄".to_string()))
+                            .description("Skills scores"),
+                        CreateSelectMenuOption::new("Trueskill", "trueskill")
+                            .emoji(ReactionType::Unicode("📊".to_string()))
+                            .description("TrueSkill ranking from vrc-data-analysis"),
+                        CreateSelectMenuOption::new("Events", "events")
+                            .emoji(ReactionType::Unicode("🗓️".to_string()))
+                            .description("Event attendance from this team"),
+                    ],
+                },
+            );
+            let season_menu = CreateSelectMenu::new(
+                "team_season_select",
+                CreateSelectMenuKind::String {
+                    options: vec![
+                        CreateSelectMenuOption::new("Season 1", "opt_1").description("2022-2023"),
+                        CreateSelectMenuOption::new("Season 2", "opt_2")
+                            .description("2023-2024")
+                            .default_selection(true),
+                    ],
+                },
+            );
+
             let message = CreateInteractionResponseMessage::new()
                 .components(vec![
                     CreateActionRow::SelectMenu(page_menu),
@@ -65,16 +72,31 @@ pub async fn response(_ctx: &Context, interaction: &CommandInteraction, roboteve
                         .title(format!("Team {}", team.number))
                         .description(team.team_name)
                         .field("Organization", team.organization, true)
-                        .field("Grade", team.grade, true)    
-                        .field("Registered", format!("{}", team.registered), true)
+                        .field("Program", team.grade, true)
+                        .field(
+                            "Registered",
+                            if team.registered { "Yes" } else { "No" },
+                            true,
+                        )
+                        .field("Robot", team.robot_name, true)
+                        .field(
+                            "Location",
+                            format!("{}, {}, {}", team.location.city, team.location.region, team.location.country),
+                            true
+                        ),
                 );
 
-            CreateInteractionResponse::Message(message)   
+            CreateInteractionResponse::Message(message)
         } else {
-            return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("Failed to get information about team from RobotEvents."))
+            return CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::new()
+                    .content("Failed to get information about team from RobotEvents."),
+            );
         }
     } else {
-        return CreateInteractionResponse::Message(CreateInteractionResponseMessage::new().content("Failed to connect to RobotEvents."))
+        return CreateInteractionResponse::Message(
+            CreateInteractionResponseMessage::new().content("Failed to connect to RobotEvents."),
+        );
     }
 }
 
@@ -82,6 +104,7 @@ pub fn register() -> CreateCommand {
     CreateCommand::new("team")
         .description("Embed Test")
         .add_option(
-            CreateCommandOption::new(CommandOptionType::String, "team", "Team Number").required(true)
+            CreateCommandOption::new(CommandOptionType::String, "team", "Team Number")
+                .required(true),
         )
 }
